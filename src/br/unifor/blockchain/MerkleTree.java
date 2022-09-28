ecd597875e9445784921891e9de942833ceb47cb
Felipe Dias <felipemacdowell@unifor.br>
2022-09-28 16:04:34 -0300

    Adiconando arvore de merkle

---
+++ src/br/unifor/blockchain/MerkleTree.java
@@ -0,0 +1,97 @@
+package br.unifor.blockchain;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MerkleTree {
+
+    public static Node generateTree(ArrayList<String> dataBlocks) {
+        ArrayList<Node> childNodes = new ArrayList<>();
+
+        for (String message : dataBlocks) {
+            childNodes.add(new Node(null, null, calculateBlockHash(message)));
+        }
+
+        return buildTree(childNodes);
+    }
+    
+    public static String calculateBlockHash(String message) {
+        MessageDigest digest = null;
+        byte[] bytes = null;
+        
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+            bytes = digest.digest(message.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+        }
+        StringBuffer buffer = new StringBuffer();
+        for (byte b : bytes) {
+            buffer.append(String.format("%02x", b));
+        }
+        return buffer.toString();
+    }
+
+    private static Node buildTree(ArrayList<Node> children) {
+        ArrayList<Node> parents = new ArrayList<>();
+
+        while (children.size() != 1) {
+            int index = 0, length = children.size();
+            while (index < length) {
+                Node leftChild = children.get(index);
+                Node rightChild = null;
+
+                if ((index + 1) < length) {
+                    rightChild = children.get(index + 1);
+                } else {
+                    rightChild = new Node(null, null, leftChild.getHash());
+                }
+
+                String parentHash = calculateBlockHash(leftChild.getHash() + rightChild.getHash());
+                parents.add(new Node(leftChild, rightChild, parentHash));
+                index += 2;
+            }
+            children = parents;
+            parents = new ArrayList<>();
+        }
+        return children.get(0);
+    }
+
+    private static void printLevelOrderTraversal(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        if ((root.getLeft() == null && root.getRight() == null)) {
+            System.out.println(root.getHash());
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (node != null) {
+                System.out.println(node.getHash());
+            } else {
+                System.out.println();
+                if (!queue.isEmpty()) {
+                    queue.add(null);
+                }
+            }
+
+            if (node != null && node.getLeft() != null) {
+                queue.add(node.getLeft());
+            }
+
+            if (node != null && node.getRight() != null) {
+                queue.add(node.getRight());
+            }
+
+        }
+
+    }
+}
